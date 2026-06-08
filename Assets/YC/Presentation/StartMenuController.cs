c169e2d7e23c13510a5e5c522e73807b1767f99b
卜元 胡 <1322062107@qq.com>
2026-06-08 18:00:20 +0800

    Fix start menu cover and button

--- Assets/YC/Presentation/StartMenuController.cs
+++ Assets/YC/Presentation/StartMenuController.cs
@@ -63,16 +63,16 @@ namespace YC.Presentation
             rectTransform.anchorMin = new Vector2(0.5f, 0f);
             rectTransform.anchorMax = new Vector2(0.5f, 0f);
             rectTransform.pivot = new Vector2(0.5f, 0.5f);
-            rectTransform.anchoredPosition = new Vector2(0f, 120f);
-            rectTransform.sizeDelta = new Vector2(300f, 72f);
+            rectTransform.anchoredPosition = new Vector2(0f, 245f);
+            rectTransform.sizeDelta = new Vector2(640f, 140f);
 
             var image = buttonObject.GetComponent<Image>();
-            image.color = new Color(0.08f, 0.1f, 0.12f, 0.92f);
+            image.color = new Color(0.12f, 0.08f, 0.05f, 0.18f);
 
             var button = buttonObject.GetComponent<Button>();
             button.onClick.AddListener(StartGame);
 
-            var textObject = new GameObject("Text", typeof(RectTransform), typeof(Text));
+            var textObject = new GameObject("Text", typeof(RectTransform), typeof(Text), typeof(Outline));
             textObject.transform.SetParent(buttonObject.transform, false);
 
             var textRect = textObject.GetComponent<RectTransform>();
@@ -84,9 +84,14 @@ namespace YC.Presentation
             var text = textObject.GetComponent<Text>();
             text.text = "开始游戏";
             text.alignment = TextAnchor.MiddleCenter;
-            text.color = Color.white;
-            text.fontSize = 32;
-            text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+            text.color = new Color(0.86f, 0.75f, 0.55f, 1f);
+            text.fontSize = 92;
+            text.fontStyle = FontStyle.Bold;
+            text.font = Font.CreateDynamicFontFromOSFont(new[] { "SimHei", "Microsoft YaHei", "Arial" }, text.fontSize);
+
+            var outline = textObject.GetComponent<Outline>();
+            outline.effectColor = new Color(0.12f, 0.08f, 0.04f, 0.95f);
+            outline.effectDistance = new Vector2(5f, -5f);
         }
 
         private static void EnsureEventSystem()
